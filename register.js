93c8e2c6a26859a10865edfc8b8d07c766fc8978
iriquus <32891445+iriquus@users.noreply.github.com>
2021-09-26 01:15:49 -0600

    Utilized Promise.all() Switched to Promise.all() instead of awaiting each register put and set.

--- register.js
+++ register.js
@@ -2,33 +2,37 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const config = require('./config.json');
 const CLIENT_ID = process.env.CLIENT_ID;
-// const GUILD_ID = config.testServer;
 const guilds = config.registeredServers;
 
-/*
-    assumes client is available in this context and that
-    client#commands exists according to earlier guide sections
-*/
 async function start( client ) {
     const commands = client.commands.map( ({ execute, ...data }) => data );
-
     const rest = new REST({ version: '9' }).setToken( process.env.TOKEN );
 
+    const queue = [];
+    console.log('Started refreshing application (/) commands.');
     guilds.forEach( async GUILD_ID => {
         try {
-            console.log('GUILDID: [' + GUILD_ID + '] Started refreshing application (/) commands.');
-            await client.guilds.cache.get(GUILD_ID)?.commands.set(commands);
-            await rest.put(
-                Routes.applicationGuildCommands( CLIENT_ID, GUILD_ID ),
-                { body: commands },
-            );
-            console.log('Successfully reloaded application (/) commands.');
 
-            await require('./permissions.js').register( client, GUILD_ID );
+            console.log(`[Guild: (${GUILD_ID}) Started refreshing application (/) commands.`);
+            
+            queue.push(
+                client.guilds.cache.get(GUILD_ID)?.commands.set(commands)
+            );
+            queue.push(
+                rest.put(
+                    Routes.applicationGuildCommands( CLIENT_ID, GUILD_ID ),
+                    { body: commands },
+                )
+            );
     
-        } catch (error) {
+        }
+        catch ( error ) {
             console.error(error);
         }
+
+        await Promise.all( queue );
+        console.log('Successfully reloaded application (/) commands.');
+
     });
 }
 
