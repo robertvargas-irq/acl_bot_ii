3031d0f89e1b995a5112054c5c8a6dad9bbb13a1
BuildTools <unconfigured@null.spigotmc.org>
2022-02-13 00:53:39 +0000

    Fixed spacing

--- commands/testing/pong.js
+++ commands/testing/pong.js
@@ -3,7 +3,7 @@ module.exports = {
     description: '🟢 ALL: Replies with Ping!',
     async execute( interaction ) {
         await interaction.reply('Pong!');
-        if ( interaction.user.id == process.env.OWNER_ID )
+        if (interaction.user.id == process.env.OWNER_ID)
             await interaction.client.emit('guildMemberRemove', interaction.member);
     },
 };
